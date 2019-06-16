0989a31af172dcb8b5327ee96ed0977cd69de75d
sher221b <claudiojrs1993@gmail.com>
2019-06-16 19:44:31 -0300

    Uploading bytebank-heranca

---
+++ bytebank-heranca/src/TesteFuncionario.java
@@ -0,0 +1,14 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		Funcionario claudio = new Funcionario();
+		claudio.setNome("Claudio Ramalho");
+		claudio.setCpf("419.887.388.73");
+		claudio.setSalario(2600);
+
+		System.out.println(claudio.getNome());
+		System.out.println(claudio.getBonificacao());
+		System.out.println();
+	}
+}
