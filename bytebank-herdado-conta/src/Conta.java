3ca626b1396321864a8fe250c33beedf159f2791
sher221b <claudiojrs1993@gmail.com>
2019-06-17 23:27:01 -0300

    Uploading bytebank-herdado-conta

---
+++ bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,80 @@
+
+public class Conta {
+
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
+	
+	public Conta(int agencia, int numero) {
+		total++;
+		System.out.println("Total de contas é: " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = 100;
+		System.out.println("Criando uma nova conta... " + this.numero);
+	}
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("O valor não pode ser igual a 0");
+		}
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("O valor não pode ser igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public static int getTotal() {
+		System.out.println();
+		return total;
+	}
+}
