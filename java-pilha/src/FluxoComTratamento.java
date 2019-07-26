ff9d61a8ebf07be4da8b14a5b0988608cb7d86ae
sher221b <claudiojrs1993@gmail.com>
2019-07-25 22:42:00 -0300

    Updating "Lançando exceções"

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,5 +1,5 @@
 
-public class Fluxo {
+public class FluxoComTratamento {
 	
 	public static void main(String[] args) {
 		
@@ -17,19 +17,14 @@ public class Fluxo {
 	private static void metodo1() {
 		
 		System.out.println("Ini do metodo1");
-			metodo2();
-
+		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 	
 	private static void metodo2() {
 		
 		System.out.println("Ini do metodo2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
-			int a = i / 0;
-		}
-		
-		System.out.println("Fim do metodo2");
+		throw new ArithmeticException("Deu errado");
+		//System.out.println("Fim do metodo2");
 	}
 }
