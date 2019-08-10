38b5879947710c8a78317e0585de494802b1b3e8
sher221b <claudiojrs1993@gmail.com>
2019-08-10 18:18:53 -0300

    ripa na chulipa la catica "Array de referências";

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteObject.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteObject.java
@@ -2,13 +2,15 @@ package br.com.bytebank.banco.test;
 
 import br.com.bytebank.banco.modelo.*;
 
-public class Teste {
+public class TesteObject {
 	
 	public static void main(String[] args) {
 		
 		ContaCorrente cc = new ContaCorrente(22, 33);
 		ContaPoupanca cp = new ContaPoupanca(33, 22);
 		Object cliente = new Cliente();
+		
+		cliente.toString();
 		
 		System.out.println(cc.toString());
 		System.out.println(cp);
